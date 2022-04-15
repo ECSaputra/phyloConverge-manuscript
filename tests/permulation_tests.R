26657ed95c307d772958438b9ed42f61b2d2113a
ECSaputra <elysia.cecilia@gmail.com>
2022-04-15 14:34:36 -0400

    edited permulation functions
    edited permulation functions

--- tests/permulation_tests.R
+++ tests/permulation_tests.R
@@ -1,17 +1,6 @@
 library(RERconverge)
 source("../phenotype_tree_processing.R")
-
-
-getPathvecfromForegroundTree=function(foreground_tree){
-  fg_edge_idx = which(foreground_tree$edge.length==1)
-  fg_node_idx = foreground_tree$edge[fg_edge_idx,2]
-  foreground_species = foreground_tree$tip.label[fg_node_idx]
-  
-  pathvec = rep(0, length(foreground_tree$tip.label))
-  names(pathvec) = foreground_tree$tip.label
-  pathvec[foreground_species] = 1
-  pathvec
-}
+source("../permulation_functions.R")
 
 
 neutraltree = read.tree("../toydatasets/neutraltree_24way.tree")
@@ -19,6 +8,10 @@ foregrounds = scan("../toydatasets/subterranean_foregrounds.txt", what="characte
 root_species = "mm10"
 
 num_perms = 10
+
+test = getPermulatedPhenotypes(foregrounds, neutraltree, 20, "mm10")
+
+
 
 
 getPermulatedPhenotypeTree=function(foregrounds, neutraltree, root_species){
@@ -32,3 +25,7 @@ getPermulatedPhenotypeTree=function(foregrounds, neutraltree, root_species){
   permulated_tree = simBinPhenoSSM(foreground_tree, masterTree, root_species, foregrounds, sisters_list=NULL, pathvec, plotTreeBool = F)
   permulated_tree
 }
+
+
+
+
